61a7e1379214489a742b2e500cfc98fc920fa14d
tiero <3596602+tiero@users.noreply.github.com>
2023-06-08 15:29:03 +0200

    use right port 8000

--- install.sh
+++ install.sh
@@ -209,7 +209,7 @@ else
 fi
 
 echo -e "🎉 Congratulations! Your Prem instance is ready to use"
-echo "Please visit http://$(curl -4s https://ifconfig.io):8080 to get started."
+echo "Please visit http://$(curl -4s https://ifconfig.io):8000 to get started."
 
 curl --silent -X POST https://analytics.prem.ninja/api/event \
     -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.121 Safari/537.36 OPR/71.0.3770.284' \
