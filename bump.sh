5811a20bd0f24e89d40a40bfda44db96b2cfbf7e
tiero <3596602+tiero@users.noreply.github.com>
2023-11-03 01:17:16 +0100

    bump.sh: fix version tag

--- bump.sh
+++ bump.sh
@@ -31,19 +31,20 @@ bump_to_latest_tag() {
     latest_release=$(curl -s https://api.github.com/repos/${owner}/${1}/releases/latest | jq -r .tag_name)
     echo "Latest release tag: $latest_release"
 
-    # Assuming Docker image tag follows the same naming convention as the GitHub tag
     # Pull the Docker image from GitHub Container Registry
-    image=ghcr.io/${owner}/${3}:$latest_release
-    echo "Pulling image: $image"
-    docker pull ${image} &> /dev/null
+    image_with_tag=ghcr.io/${owner}/${3}:${latest_release}
+    echo "Pulling image: $image_with_tag"
+    docker pull ${image_with_tag} &> /dev/null
 
     # Get the Docker image digest
-    image_digest=$(docker inspect --format='{{.RepoDigests}}' ghcr.io/${owner}/${3}:$latest_release | awk -F '@' '{print $2}' | tr -d '[]')
+    image_digest=$(docker inspect --format='{{.RepoDigests}}' ${image_with_tag} | awk -F '@' '{print $2}' | tr -d '[]')
     echo "Image digest: $image_digest"
 
+    # Store only the base image name without the tag
+    base_image=ghcr.io/${owner}/${3}
 
-    # Update JSON with new tag, digest, and image
-    jq --arg rep "$2" --arg ver "$latest_release" --arg img "$image" --arg dig "$image_digest" \
+    # Update JSON with new tag, digest, and base image name
+    jq --arg rep "$2" --arg ver "$latest_release" --arg img "$base_image" --arg dig "$image_digest" \
     '.prem[$rep].version = $ver | .prem[$rep].digest = $dig | .prem[$rep].image = $img' $json_file > temp.json && mv temp.json $json_file
 }
 
@@ -53,16 +54,34 @@ bump_to_latest_tag $dnsd_repo $dnsd $dnsd_image
 bump_to_latest_tag $controllerd_repo $controllerd $controllerd_image
 bump_to_latest_tag $auth_repo $auth $auth_image
 
+
+# Function to ask for user confirmation
+confirm_commit() {
+    while true; do
+        read -p "Do you want to commit and push changes? (yes/no): " yn
+        case $yn in
+            [Yy]* ) break;;
+            [Nn]* ) echo "Exiting without committing."; exit;;
+            * ) echo "Please answer yes or no.";;
+        esac
+    done
+}
+
+# ... [previous code for bump_to_latest_tag calls]
+
 # Check for changes in the repository
 if [ -z "$(git diff -- $json_file)" ]; then
     echo "No changes detected."
     exit 0
 fi
+
+# Ask for user confirmation
+confirm_commit
+
 ## Commit & Push to main
 git add $json_file
 git commit -S -m "Bump to latest releases"
 git push origin main
-
 
 
 
